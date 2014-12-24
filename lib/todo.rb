c96b8feed7c3e6a825ef1554086f18a18a193661
Ben Ridgway <benridgway83@gmail.com>
2014-12-24 17:39:09 +0900

    implement new task logic

--- lib/todo.rb
+++ lib/todo.rb
@@ -2,3 +2,9 @@ require 'todo/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+
+def read_todos(filename)
+
+	File.open(filename,'r').readlines 
+
+end
